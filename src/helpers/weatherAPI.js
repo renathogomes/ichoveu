7ce8ce97815236d978b9e7d8a396910080e7989f
renatho <renatho@pop-os.localdomain>
2023-05-25 17:53:11 -0300

    primeiro requisito feito

--- src/helpers/weatherAPI.js
+++ src/helpers/weatherAPI.js
@@ -1,7 +1,12 @@
-// Remova os comentários a medida que for implementando as funções
+const TOKEN = import.meta.env.VITE_TOKEN;
 
-export const searchCities = (/* term */) => {
-//   seu código aqui
+export const searchCities = (term) => {
+  if (!term) {
+    window.alert('Nenhuma cidade encontrada');
+  }
+  fetch(`http://api.weatherapi.com/v1/search.json?lang=pt&key=${TOKEN}&q=${term}`)
+    .then((rest) => rest.json())
+    .then((data) => console.log(data));
 };
 
 export const getWeatherByCity = (/* cityURL */) => {
